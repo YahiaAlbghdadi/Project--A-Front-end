f3607f41e4f2e047561d4eaa31475f08de9aac37
YahiaAlbghdadi <r0y4lalbghdadi@gmail.com>
2021-11-20 14:07:06 +0100

    components added

--- projectA/src/app/app.module.ts
+++ projectA/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { ContentComponent } from './content/content.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FooterComponent } from './footer/footer.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    ContentComponent,
+    CoursesComponent,
+    FooterComponent,
+    CourseDetailsComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
